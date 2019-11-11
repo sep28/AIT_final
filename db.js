b5d7704ac6d4af361bd1f967800130157fd9e556
Sep <sepmacbook@10-16-105-101.dynapool.wireless.nyu.edu>
2019-11-10 19:49:08 -0500

    updating some stuff from milestone 2

--- db.js
+++ db.js
@@ -32,25 +32,26 @@ mongoose.model('Player', Player);
 // is the environment variable, NODE_ENV, set to PRODUCTION? 
 let dbconf;
 if (process.env.NODE_ENV === 'PRODUCTION') {
- // if we're in PRODUCTION mode, then read the configration from a file
- // use blocking file io to do this...
- const fs = require('fs');
- const path = require('path');
- const fn = path.join(__dirname, 'config.json');
- const data = fs.readFileSync(fn);
+	 // if we're in PRODUCTION mode, then read the configration from a file
+ 	// use blocking file io to do this...
+ 	const fs = require('fs');
+ 	const path = require('path');
+ 	const fn = path.join(__dirname, 'config.json');
+ 	const data = fs.readFileSync(fn);
 
- // our configuration file will be in json, so parse it and set the
- // conenction string appropriately!
- const conf = JSON.parse(data);
- dbconf = conf.dbconf;
+ 	// our configuration file will be in json, so parse it and set the
+ 	// conenction string appropriately!
+ 	const conf = JSON.parse(data);
+ 	dbconf = conf.dbconf;
+
 } else {
- // if we're not in PRODUCTION mode, then use
- dbconf = 'mongodb://localhost/sa3736';
+ 	// if we're not in PRODUCTION mode, then use
+ 	dbconf = 'mongodb://localhost/sa3736';
 }
 
 mongoose.connect(dbconf, {useNewUrlParser: true}, function(err) {
   if (err) {
-    console.log("Could not connect to mongodb ");
+	console.log("Could not connect to mongodb ");
   }
   else {
     console.log("Connected to mongodb");
